c92557a50835a73e67df7c3dd5600a6c01a11c0f
KesseT <64742407+KetCode@users.noreply.github.com>
2024-08-04 12:14:28 -0300

    feat: create a list to see the names correspondentes with the input

--- js/pokemon-search.js
+++ js/pokemon-search.js
@@ -1,15 +1,26 @@
-document.getElementById('search-button').addEventListener('click', (e) => {
-    e.preventDefault();
-    const searchInput = document.getElementById('search-input');
-    const pokemonName = searchInput.value.trim();
+const searchInput = document.getElementById('search-input');
+const suggestionsList = document.getElementById('suggestions-list');
+let debounceTimeout = null;
+
+searchInput.addEventListener('input', () => {
+  const pokemonName = searchInput.value.trim(); // trim metodo que remove espaços entre ambos (inicio e fim) | It removes any whitespace characters (spaces, tabs, line breaks, etc.) from the start and end of the string
   
-    if (pokemonName) {
-      pokeApi.getPokemonByName(pokemonName)
-        .then((pokemon) => {
-          console.log(pokemon);
+  if (pokemonName.length >= 3) {
+    clearTimeout(debounceTimeout); // Prevent a function from running multiple times and Cancel a timer that is no longer needed
+    debounceTimeout = setTimeout(() => {
+      pokeApi.searchPokemonsByNames(pokemonName)
+        .then((pokemons) => {
+          const suggestions = pokemons.map((pokemon) => pokemon.name);
+          suggestionsList.innerHTML = '';
+          suggestions.forEach((suggestion) => {
+            const suggestionElement = document.createElement('li');
+            suggestionElement.textContent = suggestion;
+            suggestionsList.appendChild(suggestionElement);
+          });
         })
         .catch((error) => {
           console.error(error);
         });
-    }
-  });
+    }, 500);
+  }
+});
